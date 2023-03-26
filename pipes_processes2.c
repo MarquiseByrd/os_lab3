9bcfb26049261777a2c62775dac0d4fab0c91436
MarquiseByrd <72713442+MarquiseByrd@users.noreply.github.com>
2023-03-26 13:48:50 -0400

    Update pipes_processes2.c

--- pipes_processes2.c
+++ pipes_processes2.c
@@ -1,3 +1,4 @@
+//Marquise Byrd @02939181
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -19,42 +20,20 @@ int main(int argc, char **argv)
   char *cat_args[] = {"cat", "scores", NULL};
   char *grep_args[] = {"grep", "Lakers", NULL};
 
-  // make a pipe (fds go in pipefd[0] and pipefd[1])
-
-  pipe(pipefd);
-
+  
+  pipe(pipefd); // creates a pipe 
   pid = fork();
 
   if (pid == 0)
     {
-      // child gets here and handles "grep Villanova"
-
-      // replace standard input with input part of pipe
-
-      dup2(pipefd[0], 0);
-
-      // close unused hald of pipe
-
-      close(pipefd[1]);
-
-      // execute grep
-
-      execvp("grep", grep_args);
+      dup2(pipefd[0], 0); 
+      close(pipefd[1]); // close part of pipe that is unused
+      execvp("grep", grep_args); // execute grep
     }
   else
     {
-      // parent gets here and handles "cat scores"
-
-      // replace standard output with output part of pipe
-
-      dup2(pipefd[1], 1);
-
-      // close unused unput half of pipe
-
-      close(pipefd[0]);
-
-      // execute cat
-
-      execvp("cat", cat_args);
+      dup2(pipefd[1], 1); // standard output is replaced with output 
+      close(pipefd[0]); // close unused unput half of pipe
+      execvp("cat", cat_args); // execute cat
     }
 }
